039446a04ac9d38c530c451568ddb52fd54c3c89
rokilianolandi <rokilianolandi.gmail.com>
2020-07-06 22:10:22 +0700

    belajar membuat objek & memanggil objek tersebut baris 39/46

--- falidasi.js
+++ falidasi.js
@@ -1,17 +1,22 @@
+
 function waktu() {
     var x = document.getElementById("exampleInputEmail1").value;
     var y = document.getElementById("exampleInputPassword1").value;
     var z = document.getElementById("exampleCheck1").checked;
-
     if (x == "" && y == "" && z == null) {
         alert("salah");
+        document.getElementById("exampleInputEmail1").onfocus();
         return false;
     } else if (x == "") {
         alert("Email Kosong");
-        return true;
+        return false;
     } else if (y == "") {
         alert("Password Kosong");
-        return true;
+        return false;
+    }
+    else{
+        alert("Welcome");
+        return false;
     }
 }
 document.getElementById("hallo").addEventListener("click", apa);
@@ -26,5 +31,17 @@ function apa() {
 //membuat option value
 var bahasa = document.getElementById("bahasa");
 document.getElementById("Submit-Bahasa").onclick = function () {
- alert(bahasa[bahasa.selectedIndex].value);
+ //console.log(bahasa[bahasa.selectedIndex].value);
+alert (bahasa[bahasa.selectedIndex].text);
 }
+
+//properti objek
+var User ={
+    nama :"flandy",
+    kelas : 3,
+    id : 1234567,
+    fullName : function(){
+        return this.nama.indexOf("l");
+    }
+};
+//document.getElementById("text").innerHTML = User.fullName();
